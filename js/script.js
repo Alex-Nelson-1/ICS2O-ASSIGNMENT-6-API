8c5009a9099a2914b3094d1efb0e5feeac6720de
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2023-01-21 17:48:20 +0000

    Quotes and Authors appear

--- js/script.js
+++ js/script.js
@@ -1,21 +1,48 @@
 // Created by: Alex Nelson
-// Created on: Oct 2022
+// Created on: Jan 2022
 // This file contains the JS functions for index.html
 
 "use strict"
 
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-ASSIGNMENT-6-API-1/sw.js", {
+    scope: "/ICS2O-ASSIGNMENT-6-API-1/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function displays a different quote evry time the page is reloaded.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+
+const getWeather = async (URLAddress) => {
+  try {
+
+    //Code to get Api
+    const result = await fetch(URLAddress)
+    const jsonData = await result.json()
+    console.log(jsonData)
+
+    //Expression randomizing the Quotes
+    if (jsonData) {
+        const randomizer = Math.floor(Math.random() * 1642)
+
+      //Variables for quotes and author
+        const quote = jsonData[randomizer].text
+        const author = jsonData[randomizer].author
+
+      //Display Quotes and Author
+     document.getElementById("quote-author").innerHTML = "Quote: " + quote + "<br> <br>" + "Author: " + author 
+  } else {
+    console.log(error)
+  }
+    //Catch Errors
+  } catch (err) {
+    console.log(err)
+  }
 }
+
+//Api link
+getWeather("https://type.fit/api/quotes")
